2cd30a71401e755220928c3420213b8336f9f498
Vannak-Thea <vannak.thea@techbodia.com>
2022-10-10 17:40:59 +0700

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <HeaderBlock></HeaderBlock>
+  <HomePage></HomePage>
+</template>
+
+<script lang="ts">
+import HomePage from "./components/HomePage.vue";
+import HeaderBlock from "./components/HeaderBlock.vue";
+
+export default {
+  name: "App",
+  components: {
+    HomePage,
+    HeaderBlock,
+  },
+};
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.wrap{
+    width: 1200px;
+    margin: 0 auto;
+    background: #fff;
+}
+</style>
---
+++ src/components/HeaderBlock.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrap">
+        <div class="header">
+            <div class="profile">
+                <img alt="ai" src="../assets/profile2.jpg">
+                <P class="name">THEA VANNAK</P>
+            </div>
+        
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "HeaderBlock",
+};
+</script>
+
+<style scoped lang="scss">
+.header{
+    padding: 20px 20px 0 20px;
+    position: relative;
+    border: 1px solid #ccc;
+    border-bottom: 0;
+}
+.profile{
+    display: flex;
+    justify-content: left;
+}
+.profile img{
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    object-fit: cover;
+    margin-right: 20px;
+}
+.name{
+    display: flex;
+    align-items: center;
+}
+.search {
+    position: absolute;
+    right: 20px;
+    top: 25px;
+}
+.search-txt{
+    width: 210px;
+    padding: 10px 10px;
+}
+</style>
