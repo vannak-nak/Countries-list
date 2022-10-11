283c98620a3593841a36d01a548390e310a4b1dc
Vannak-Thea <vannak.thea@techbodia.com>
2022-10-11 17:44:18 +0700

    third commit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import store from "./store";
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(store).mount("#app");
+createApp(App).use(store,VueAxios, axios).mount("#app");
