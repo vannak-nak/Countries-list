628717a7e5a537ae774061030585ed31ede5b20a
Vannak-Thea <vannak.thea@techbodia.com>
2022-10-12 17:24:41 +0700

    main file

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,7 @@ import "./registerServiceWorker";
 import store from "./store";
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-createApp(App).use(store,VueAxios, axios).mount("#app");
+createApp(App).use(store, VueAxios, axios, library, FontAwesomeIcon).mount("#app");
